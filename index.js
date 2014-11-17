8cfe2894ab972341f0e1a436ca25d377d4cbf2e4
yukjin <luzhen@sungymobile.com>
2014-11-17 10:14:03 +0800

    u

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var app=express();
-var control=require('./control.js');
+var control=require('./control');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var ips={};
